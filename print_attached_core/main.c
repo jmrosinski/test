b620ae2a1435a044ebe5164abdac6d94bf7c3a03
Jim Rosinski <jmrosinski@gmail.com>
2016-06-28 13:08:39 -0600

    o Add "-w <seconds>" option to enable user to specify time between forced   affinity changes.

--- print_attached_core/main.c
+++ print_attached_core/main.c
@@ -2,39 +2,62 @@
 #include <omp.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
 
-char status;  // status of thread
-int core;    // prv or current core on which running
-extern void run_forever (FILE *);
+int iam;
+int shiftiam;  // for shifting affinity
+int nranks;
+int pid;       // process id
+int nthreads;
+int shiftintvl = 10;    // wait this long before forcing affinity shift (default 10 seconds)
+volatile int *core;     // array of attached cores
+extern void init_core (int);
+extern void threaded_loop (void);
 extern void print_whos_running_where (FILE *, int);
 
 int main (int argc, char **argv)
 {
-  int iam;
-  int nthreads, mythread;
   int ret;
-  int n;
-  int pid, tid;
-  char path[64];
   FILE *fp;
   int c;
-  int pin2core = 0;
-  int pin2range = 0;
+  int pin2core = 0;    // 1=> pin threads to single core
+  int pin2range = 0;   // 1=> pin threads to range of cores
+  int cpn = 1;         // default cores per node
 
   ret = MPI_Init (&argc, &argv);
   ret = MPI_Comm_rank (MPI_COMM_WORLD, &iam);
+  shiftiam = iam;
+  ret = MPI_Comm_size (MPI_COMM_WORLD, &nranks);
+
   pid = getpid ();
   nthreads = omp_get_max_threads ();
+  core = malloc (nthreads * sizeof (int));
+  init_core (nthreads);
 
-  while ((c = getopt (argc, argv, "cr")) != -1) {
+  while ((c = getopt (argc, argv, "hcrn:w:")) != -1) {
     switch (c) {
+    case 'h':
+      if (iam == 0) {
+	printf ("Usage: %s [-c] to pin to single core\n"
+		"          [-r] to pin to range of cores\n"
+		"          [-n <num>] number of cores per node\n"
+		"          [-w <num> number of seconds between forced shifts", argv[0]);
+      }
+      return 0;
+      break;
     case 'c':
       pin2core = 1;
       break;
     case 'r':
       pin2range = 1;
+      break;
+    case 'n':
+      cpn = atoi (optarg);
+      break;
+    case 'w':
+      shiftintvl = atoi (optarg);
       break;
     default:
       printf ("unknown option %c\n", c);
@@ -58,11 +81,37 @@ int main (int argc, char **argv)
       printf ("No pinning\n");
     }
   }
-  ret = print_affinity (&iam);
+  ret = print_affinity_ (&iam);
 
-#pragma omp parallel for private (fp, tid, core, mythread, status)
+  while (1) {
+    // Loop for 1 second and print when attachment changes
+    threaded_loop ();
+    // Change affinity to guarantee all is working as expected
+    if (pin2core || pin2range) {
+      shiftiam = (shiftiam + 1) % nranks;
+      printf ("shiftiam=%d\n", shiftiam);
+      ret = set_affinity_ (&shiftiam, &cpn, &nthreads, &pin2core);
+    }
+    ret = print_affinity_ (&iam);
+  }
+}
+
+void threaded_loop ()
+{
+  int n;
+  int iter;
+  int ret;
+  int tid;
+  int mythread;
+  static const int usleeptime = 100000;
+  int niter = ((unsigned long) shiftintvl * 1000000) / usleeptime;
+  char path[64];
+  FILE *fp;
+
+  // Loop for shiftintvl seconds and print out if any affinities change
+#pragma omp parallel for private (fp, path, tid, mythread)
   for (n = 0; n < nthreads; ++n) {
-    //    tid = gettid ();
+    mythread = omp_get_thread_num ();
 #ifdef SYS_gettid
     tid = syscall (SYS_gettid);
 #else
@@ -71,39 +120,38 @@ int main (int argc, char **argv)
 
     sprintf (path, "/proc/%d/task/%d/stat", pid, tid);
     fp = fopen (path, "r");
-    fscanf (fp, "%*d %*s %c %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
-	                   "%*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
-	                   "%*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
-	                   "%*ld %*ld %*ld %*ld %*ld %d %*d %*d %*d %*d %*d", &status, &core);
-    printf ("thread=%d status=%c core=%d\n", mythread, status, core);
-    run_forever (fp);
-  }
-}
-
-void run_forever (FILE *fp)
-{
-  int mythread = omp_get_thread_num ();
-
-  while (1) {
-    print_whos_running_where (fp, mythread);
-    usleep (1000);
+      
+    for (iter = 0; iter < niter; ++iter) {
+      print_whos_running_where (fp, mythread);
+      usleep (usleeptime);
+    }
+    if (fclose (fp)) {
+      printf ("Failed to close path=%s\n", path);
+      ret = MPI_Abort (MPI_COMM_WORLD, -1);
+    }
   }
 }
 
 void print_whos_running_where (FILE *fp, int mythread)
 {
-  char newstatus;
+  char status;
   int newcore;
 
   rewind (fp);
   fscanf (fp, "%*d %*s %c %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
         	         "%*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
 	                 "%*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld %*ld "
-	                 "%*ld %*ld %*ld %*ld %*ld %d %*d %*d %*d %*d %*d", &newstatus, &newcore);
-  if (newcore != core) {
-    printf ("thread=%d status=%c core=%d\n", mythread, newstatus, newcore);
-    status = newstatus;
-    core = newcore;
+	                 "%*ld %*ld %*ld %*ld %*ld %d %*d %*d %*d %*d %*d", &status, &newcore);
+  if (newcore != core[mythread]) {
+    printf ("iam=%d thread=%d status=%c core=%d\n", iam, mythread, status, newcore);
+    core[mythread] = newcore;
   }
 }
-  
+
+void init_core (int nthreads)
+{
+  int n;
+
+  for (n = 0; n < nthreads; ++n)
+    core[n] = -1;
+}
