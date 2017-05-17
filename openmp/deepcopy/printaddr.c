884fad3d907d6f7c2b4de45122785a89abab0d08
Jim Rosinski <jmrosinski@gmail.com>
2017-05-16 20:09:50 -0600

    o Print address to prove multiple copies per thread

---
+++ openmp/deepcopy/printaddr.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int *printaddr_ (int *var)
+{
+  return var;
+}
--- openmp/deepcopy/sub.F90
+++ openmp/deepcopy/sub.F90
@@ -4,8 +4,11 @@ subroutine sub (n, testit)
 
   integer, intent(in) :: n
   type (tobecopied), intent(inout) :: testit
+  integer(8), external :: printaddr
   
-  write(6,*)'Inside sub n=',n
+  write(6,100)'Inside sub n=',n,' Address of x=',printaddr(testit%x(n))
+100 format(a,i2,a,z16)
+  write(6,100)'Inside sub n=',n,' Address of z=',printaddr(testit%y(n)%z)
   write(6,*)'Inside sub testit%x(n)=', testit%x(n)
   write(6,*)'Inside sub testit%y(n)%z=', testit%y(n)%z
 end subroutine sub
