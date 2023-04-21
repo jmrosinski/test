719be9151465978301858a6f2b6bf97319cedba1
jmrosinski <jmrosinski@gmail.com>
2023-04-20 20:50:04 -0600

    eliminate pesky warnings

--- emulate_gptl_cuda/cacctests/basic.c
+++ emulate_gptl_cuda/cacctests/basic.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <cuda.h>
 #include "../src/gptl.h"
 #include "../cuda/src/gptl_acc.h"
 
 int main ()
 {
   int n, ret;
-  int zero = 0;
 
   ret = GPTLstart (0);
 #pragma acc parallel private(ret) copyin(n)
