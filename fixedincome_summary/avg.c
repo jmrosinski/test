46de77bb03801de15dfcfb4497d8c2a66536ca75
jmrosinski <jmrosinski@gmail.com>
2023-10-15 16:08:26 -0600

    Modified avg.c to account for taxes and basis (new entries in input file   required). Modified avg.c to account for inflation as well (user input from cmd line) These changes effective with input data 10/13/23 (old code won't work with   new data files and new code won't work with old data files)

--- fixedincome_summary/avg.c
+++ fixedincome_summary/avg.c
@@ -4,12 +4,17 @@
 
 int main ()
 {
-  char line[LINESIZE];   // input line
+  char line[LINESIZE]; // input line
   char infile[24];
   FILE *fp;
-  float rate;
-  float amt, amttot;
-  float wixitot;
+  float inflation;             // inflation (%)
+  float amt, amttot;           // current value ($)
+  float income, incometot;     // income
+  float actual, actualtot;     // income after taxes
+  float realized, realizedtot; // realized gain after taxes and inflation
+  float rate;                  // interest rate (%) 
+  float basis, basistot;       // cost basis
+  float taxrate;               // aggregate tax rate (%)
   float wbar;
   int nument;
 
@@ -23,24 +28,43 @@ int main ()
   }
   printf ("Successfully opened infile=%s\n", infile);
 
+  printf ("Enter inflation rate (percent)\n");
+  scanf ("%f", &inflation);
+  inflation *= 0.01;
+
   // Skip EXACTLY 1 LINE (header)
   fgets (line, LINESIZE, fp);
-  nument = 0;
-  amttot = 0.;
-  wixitot = 0.;
-  // Parse input lines assuming entries are: amount rate comment
+  nument      = 0;
+  amttot      = 0.;
+  incometot   = 0.;
+  actualtot   = 0.;
+  realizedtot = 0.;
+  basistot    = 0.;
+  // Parse input lines assuming entries are: amount rate basis taxrate comment
   while (fgets (line, LINESIZE, fp) != NULL) {
-    if (sscanf (line, "%f %f %*s", &amt, &rate) != 2) {
+    if (sscanf (line, "%f %f %f %f %*s", &amt, &rate, &basis, &taxrate) != 4) {
       printf ("bad input line=%s\n", line);
       return 1;
     }
+    rate    *= 0.01;
+    taxrate *= 0.01;
     ++nument;
-    amttot += amt;
-    wixitot += amt*rate;
+    income   = amt*rate;
+    actual   = income*(1. - taxrate);
+    realized = actual - inflation*amt;
+
+    amttot      += amt;
+    incometot   += income;
+    actualtot   += actual;
+    realizedtot += realized;
+    basistot    += basis;
   }
-  wbar = wixitot / amttot;
-  printf ("total fixed income=%f K\n", amttot);
-  printf ("average rate for %d entries=%f percent\n", nument, wbar);
-  printf ("yearly income=$%.2fK\n", 0.01*wbar*amttot);
+  wbar = incometot / amttot;
+  printf ("total fixed income assets=%f K\n", amttot);
+  printf ("total fixed income basis= %f K\n", basistot);
+  printf ("weighted rate for %d entries=%f percent\n", nument, 100.*wbar);
+  printf ("yearly income=$%.2fK\n", incometot);
+  printf ("yearly income after taxes taken out=$%.2fK\n", actualtot);
+  printf ("yearly income after taxes and inflation taken out=$%.2fK\n", realizedtot);
   return 0;
 }
