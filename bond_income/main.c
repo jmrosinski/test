8aaaf1c9c3c112bbf7ea98b647f7debdb906e56e
jmrosinski <jmrosinski@gmail.com>
2024-08-03 16:40:29 -0600

    Add bond_income.

---
+++ bond_income/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define LINESIZE 128  // max size of input line
+#define MAXNAME 24    // max size of fund name
+
+int get_fund_name (char *, char *);
+
+  int main ()
+{
+  char line[LINESIZE]; // input line
+  char *infile = "bond_funds";
+  FILE *fp;
+  char fund_name[MAXNAME+1];
+  double value;       // current fund value
+  double monthly;     // monthly income
+  double quarterly;   // quarterly income
+  double yearly;      // yearly income
+  double income;      // yearly income (computed)
+  double rate;        // yearly payout rate
+  double total = 0.;  // total invested in bond funds
+  int nument = 0;     // number of entries found
+
+  // Open required input file
+  if ((fp = fopen (infile, "r")) == NULL) {
+    printf ("cannot open input file %s\n", infile);
+    return 1;
+  }
+  printf ("Successfully opened infile=%s\n", infile);
+
+  // Skip over exactly 2 input header lines
+  fgets (line, LINESIZE, fp);
+  fgets (line, LINESIZE, fp);
+
+  // Parse input lines assuming entries are: fund_name value monthly quarterly
+  // If monthly > 0 use that, else quarterly
+  while (fgets (line, LINESIZE, fp) != NULL) {
+    if (get_fund_name (line, fund_name) < 0)
+      return -1;
+    if (sscanf (line, "%*s %lf %lf %lf", &value, &monthly, &quarterly) != 3) {
+      printf ("bad input line=%s\n", line);
+      return 1;
+    }
+    ++nument;
+    total += value;
+    
+    if (monthly > 0.) {
+      income = monthly*12.;
+      rate = 100.* (income / value);
+      printf ("fund=%24s value=%.1lf yearly income=%.1lf rate=%.1lf%%\n",
+	      fund_name, value, income, rate);
+    } else if (quarterly > 0.) {
+      income = quarterly*4.;
+      rate = 100.* (income / value);
+      printf ("fund=%24s value=%.1lf yearly income=%.1lf rate=%.1lf%%\n",
+	      fund_name, value, income, rate);
+    } else if (yearly > 0.) {
+      income = value;
+      rate = 100.* (income / value);
+      printf ("fund=%24s value=%.1lf yearly income=%.1lf rate=%.1lf%%\n",
+	      fund_name, value, income, rate);
+    } else {
+      printf ("fund=%s monthly quarterly or yearly value not found. Skipping \n", fund_name);
+    }
+  
+  }
+  printf ("%d entries were found totaling %.1lf\n", nument, total);
+  return 0;
+}
+
+int get_fund_name (char *line, char *fund_name)
+{
+  int len;
+  
+  while (! isalpha ((int) *line))
+    ++line;
+  for (len = 0; isalnum (line[len]) || line[len] == '_'; ++len);
+  if (len > MAXNAME) {
+    printf ("length of 1st token in %s=%d exceeds %d. Aborting\n", line, len, MAXNAME);
+    return -1;
+  }
+  for (int i = 0; i < len; ++i)
+    fund_name[i] = *line++;
+  fund_name[len] = '\0';
+  return 0;
+}
