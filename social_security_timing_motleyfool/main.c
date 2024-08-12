adba1eaff47ab2dde69c27ff7b316f5df3d761f2
jmrosinski <jmrosinski@gmail.com>
2024-08-12 10:47:36 -0600

    o Bugfix adjusts for date vs. age. Tests for starting 36 months early (0.80   FRA) and 48 months early (0.75 FRA) exactly match what's listed at motley   fool. So looks to be working correctly.

--- social_security_timing_motleyfool/main.c
+++ social_security_timing_motleyfool/main.c
@@ -1,60 +1,69 @@
 #include <stdio.h>
 
-struct FRA {
-  int yr;
-  int mo;
+struct AGE {
+  int yrs;
+  int mos;
 };
 
-void get_start (struct FRA *, const char *);
-double alter_benefits (const struct FRA, const struct FRA);
+void get_start (struct AGE *, const char *);
+double alter_benefits (const struct AGE, const struct AGE);
 
-// Find break-even age for taking social security benefits early vs. late
+// Find break-even age for starting social security benefits at 2 different times
 // Inputs:
 //   birth date (full retirement age depends on this)
 //   early starting age, late starting age (must be between 62 and 70)
 //   assumed above-inflation yearly percentage achieved by investing ss income
 // Output:
-//   age at which starting late total benefits first exceed starting early benefits, assuming
-//   all benefits are invested and earn specified yearly %
+//   age at which starting benefits later first exceeds starting benefits earlier, assuming
+//   all benefits are invested and earn user-specified yearly % (can even be 0 or negative)
 //
-// Source of adjustments to ss benefits:
+// Source of adjustments to ss benefits based on birth date and age benefits are started:
 //   www.fool.com/investing/how-much-does-waiting-claim-raise-social-security.aspx
 
 int main ()
 {
-  int birth_yr;                  // birth year
-  struct FRA fra;                // full retirement age
-  struct FRA early_start;        // early age to start benefits
-  struct FRA late_start;         // late age to start benefits for comparison
+  struct DATE {
+    int yr;
+    int mo;
+  } bdate;                       // birth date
+  struct AGE fra;                // full retirement age
+  struct AGE early_start;        // early age to start benefits
+  struct AGE late_start;         // late age to start benefits for comparison
   double early_fraction;         // early tranch adjusted from full retirement age
   double late_fraction;          // late tranch adjusted from full retirement age
   double invest_percent;         // ABOVE inflation growth from investing 100% of ss income
   double early_total;            // accum wealth starting at age early_start
   double late_total;             // accum wealth starting later than early_start
   int done, foundit;             // flag to exit loop
-  int yr, mo;                    // year and month iterators
+  int yrs, mos;                  // years and months iterators
 
-  printf ("Enter birth year but NOTE: if born Jan 1 enter previous year\n");
-  scanf ("%d", &birth_yr);
-  if (birth_yr < 1943) {
+  printf ("Enter birth date as \"year mo\" but NOTE:\n");
+  printf ("If born 1st of month enter previuos month (and year if applicable) as birth date\n");
+  scanf ("%d %d", &bdate.yr, &bdate.mo);
+  if (bdate.yr < 1943) {
     printf ("Behavior for birth years earlier than 1943 is unknown. Giving up\n");
     return 1;
-  } else if (birth_yr < 1955) {
-    fra.yr = 66;
-    fra.mo = 1;
-  } else if (birth_yr > 1959) {
-    fra.yr = 67;
-    fra.mo = 1;
+  } else if (bdate.yr < 1955) {
+    fra.yrs = 66;
+    fra.mos = 0;
+  } else if (bdate.yr > 1959) {
+    fra.yrs = 67;
+    fra.mos = 0;
   } else {
-    switch (birth_yr) {
-    case 1955: fra.yr = 66; fra.mo = 2; break;
-    case 1956: fra.yr = 66; fra.mo = 4; break;
-    case 1957: fra.yr = 66; fra.mo = 6; break;
-    case 1958: fra.yr = 66; fra.mo = 8; break;
-    case 1959: fra.yr = 66; fra.mo = 10; break;
+    fra.yrs = 66;
+    switch (bdate.yr) {
+    case 1955: fra.mos = 2; break;
+    case 1956: fra.mos = 4; break;
+    case 1957: fra.mos = 6; break;
+    case 1958: fra.mos = 8; break;
+    case 1959: fra.mos = 10; break;
+    default:
+      printf ("Unknown bdate.yr=%d\n", bdate.yr);
+      return 2;
     }
   }
-  printf ("born in %d gives fra=%d %d\n", birth_yr, fra.yr, fra.mo);
+  printf ("birth yr=%d mo=%d gives full retirement age=%d yrs %d mos\n",
+	  bdate.yr, bdate.mo, fra.yrs, fra.mos);
 
   printf ("Enter percent above inflation expected for investing soc sec income\n");
   scanf ("%lf", &invest_percent);
@@ -63,72 +72,69 @@ int main ()
     done = 1;
     get_start (&early_start, "early");
     get_start (&late_start, "late");
-    if (late_start.yr < early_start.yr ||
-      late_start.yr == early_start.yr && late_start.mo < early_start.mo) {
-      printf (" early start age=%d %d cannot exceed late start age=%d %d\n",
-	      early_start.yr, early_start.mo, late_start.yr, late_start.mo);
+    if (late_start.yrs < early_start.yrs ||
+      late_start.yrs == early_start.yrs && late_start.mos < early_start.mos) {
+      printf (" early start age=%d yrs %d mos cannot exceed late start age=%d yrs %d mos\n",
+	      early_start.yrs, early_start.mos, late_start.yrs, late_start.mos);
       done = 0;
     }
   } while (! done);
 
   early_fraction = alter_benefits (fra, early_start);
   late_fraction = alter_benefits (fra, late_start);
-  printf ("Early benefits will be changed to %.2lf of full retirement age\n", early_fraction);
-  printf ("Late benefits will be changed to %.2lf of full retirement age\n", late_fraction);
   
   early_total    = 0.;
   late_total     = 0.;
   foundit        = 0;
-  for (yr = early_start.yr; yr < 100; ++yr) {
-    mo = yr == early_start.yr ? early_start.mo : 1;
-    while (mo <= 12) {
+  for (yrs = early_start.yrs; yrs < 100; ++yrs) {
+    mos = yrs == early_start.yrs ? early_start.mos : 0;
+    while (mos < 12) {
       // *_total values are for the end of the month and year in question
       // divide by 1200 converts yearly percent to monthly fraction
       //   If per-year ss were doled out entirely on Jan 1 this number would be 0.01 (% to fraction)
-      early_total += early_fraction*(1. + invest_percent/1200.) + // income this mo tranch
+      early_total += early_fraction*(1. + invest_percent/1200.) + // income from this mo tranch
 	             early_total*(invest_percent/1200.);          // income from prv total
-      if (yr == late_start.yr && mo >= late_start.mo ||
-	  yr > late_start.yr) {
-	late_total += late_fraction*(1. + invest_percent/1200.) + // income this mo tranch
-	              late_total*(invest_percent/1200.);          // income this prv total
+      if (yrs == late_start.yrs && mos >= late_start.mos ||
+	  yrs > late_start.yrs) {
+	late_total += late_fraction*(1. + invest_percent/1200.) + // income from this mo tranch
+	              late_total*(invest_percent/1200.);          // income from prv total
       }
       if (late_total >= early_total) {
 	foundit = 1;
 	goto print_results;            // exit 2 nested loops
       }
-      ++mo;
+      ++mos;
     }
   }
   print_results:
   printf ("Results for scenario:\n");
-  printf ("Early yr mo to start benefits=%d %d\n", early_start.yr, early_start.mo);
-  printf ("Late yr mo to start benefits=%d %d\n", late_start.yr, late_start.mo);
+  printf ("Early age to start benefits=%d yrs %d mos\n", early_start.yrs, early_start.mos);
+  printf ("Late age to start benefits=%d yrs %d mos\n", late_start.yrs, late_start.mos);
   printf ("Assumed yearly percent above inflation by investing ss benefits=%.1lf\n",
 	  invest_percent);
   printf ("Early tranch=%.2lf late tranch=%.2lf\n", early_fraction, late_fraction);
 
   if (foundit)
     printf ("age=%d %d late_total=%lf first exceeds early_total=%lf\n",
-	    yr, mo, late_total, early_total);
+	    yrs, mos, late_total, early_total);
   else
     printf ("age=%d %d late_total=%lf STILL does not exceed early_total=%lf\n",
-	    yr, mo, late_total, early_total);
+	    yrs, mos, late_total, early_total);
   return 0;
 }
 
-void get_start (struct FRA *start, const char *txt)
+void get_start (struct AGE *start, const char *txt)
 {
   int done = 1;
   do {
-    printf ("Enter %s age to start taking ss income as year followed by month\n", txt);
-    scanf ("%d %d", &start->yr, &start->mo);
-    if (start->yr < 62 || start->yr > 70) {
+    printf ("Enter %s age to start taking ss income as years plus months\n", txt);
+    scanf ("%d %d", &start->yrs, &start->mos);
+    if (start->yrs < 62 || start->yrs > 70) {
       printf (" start year must be between 62 and 70\n");
       done = 0;
     }
-    if (start->mo < 1 || start->mo > 12 ||
-	(start->yr == 70 && start->mo != 1)) {
-      printf (" start month must be between 1 and 12, or =1 to start at age 70\n");
+    if (start->mos < 0 || start->mos > 11 || (start->yrs == 70 && start->mos != 0)) {
+      printf (" start months must be between 0 and 11, or =0 to start at age 70\n");
       done = 0;
     }
   } while (! done);
@@ -136,18 +142,27 @@ void get_start (struct FRA *start, const char *txt)
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
-double alter_benefits (const struct FRA fra, const struct FRA start)
+// alter_benefits()
+// apply adjustments to monthly ss benefits based on formulas from motley fool
+// Inputs:
+//   fra: full retirement age (years months)
+//   start: age to start benefits (years months)
+// Return value: fraction of full retirement benefits to be received.
+//   If start < fra, fraction will be < 1. If start > fra, fraction will be > 1.
+double alter_benefits (const struct AGE fra, const struct AGE start)
 {
-  double fraction;  // return value: positive means starting later than FRA, negative earlier
+  double fraction;  // return value: <1 means starting earlier than fra, >1 means later
   int nmo;          // number of months varied from fra
 
   fraction = 1.;    // Value for start = fra
-  nmo = (start.yr - fra.yr)*12 + (start.mo - fra.mo);
+  nmo = (start.yrs - fra.yrs)*12 + (start.mos - fra.mos);
   if (nmo > 0) {
-    // benefits will be increased. 0.01 converts % to fraction
+    // benefits will be increased from fra due to earlier start. 0.01 converts % to fraction
+    // equation from motley fool
     fraction += (0.01*2./3.) * (double) nmo;
-  } else {
-    // benefits will be reduced. 0.01 converts % to fraction
+  } else if (nmo < 0) {
+    // benefits will be reduced from fra due to later start. 0.01 converts % to fraction
+    // equation from motley fool
     nmo = -nmo;
     fraction -= (0.01*5./9.) * (double) MIN (36, nmo);
     if (nmo > 36)
