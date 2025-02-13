7a6da268d91a6e8f0405b9f52dc9a74bf203dc32
jmrosinski <jmrosinski@gmail.com>
2025-02-13 10:28:04 -0700

    o Major rewrite:   - Now 3 scenarios: Convert to Roth, Convert to aftertax now, Convert to     aftertax at end of specified period   - Capture both initial value of trad. IRA/401K and inital taxes paid     on it (where applicable)   - For new aftertax at end of specified period, capture taxes paid at end     of period

--- roth_vs_aftertax/main.c
+++ roth_vs_aftertax/main.c
@@ -2,15 +2,20 @@
 
 int main ()
 {
-  double startamt; // initial investment value
-  double growth;   // yearly percentage average growth of investment
-  double holdyrs;  // years to hold investment
-  double taxrate;  // yearly tax rate (percent) assumed constant over time
-  double rothamt;  // Roth value over time (taxed at start but not again)
-  double atamt;    // Left in taxable account but taxed after holdyrs
-  double frac;     // Fractional time in case holdyrs is not an integer
-  double attax;    // tax on atamt at end of holdyrs
-  double atfinal;  // atamt after tax applied at end of holdyrs
+  double startamt;    // initial investment value from traditional IRA or 401K
+  double growth;      // yearly average percentage growth of investment
+  double holdyrs;     // years to hold investment
+  double taxrate;     // yearly tax rate (percent). Assumed constant over time
+  double init_tax;    // taxes paid on initial conversion (Roth or aftertax)
+  double roth_now;    // Convert IRA or 401K immediately to Roth
+  double aftax_now;   // Convert IRA or 401K immediately to after tax acct
+  double aftax_later; // Convert IRA or 401K to after tax AFTER holdyrs
+  double basis;       // basis for aftax_later (only growth taxed after holdyrs)
+  double frac;        // Fractional years in case holdyrs is not an integer
+  double now_tax;     // tax on aftax_now after holdyrs
+  double now_final;   // final value of aftax_now after all taxes
+  double later_tax;   // tax when converting IRA at end of holdyrs
+  double later_final; // final value of aftax_later after all taxes
   
   printf ("Enter starting amount (thousands of $)\n");
   scanf ("%lf", &startamt);
@@ -20,23 +25,40 @@ int main ()
   scanf ("%lf", &holdyrs);
   printf ("Enter effective tax rate on income (%%)\n");
   scanf ("%lf", &taxrate);
+  taxrate *= 0.01;  // convert % to fraction
+  growth  *= 0.01;  // convert % to fraction
 
-  rothamt = startamt*(1. - taxrate*0.01);
-  atamt   = startamt;
+  // all methods require paying taxes at some point. Only aftax_later avoids it initially
+  // Only Roth avoids at end of holdyrs
+  init_tax    = startamt*taxrate;
+  roth_now    = startamt - init_tax;
+  aftax_now   = startamt - init_tax;
+  aftax_later = startamt;
+
+  basis = aftax_now;                // needed later for tax calculation
+
   for (int start = 0; start < (int) holdyrs; ++start) {
-    rothamt += growth*0.01*rothamt;
-    atamt   += growth*0.01*atamt;
+    roth_now    += growth*roth_now;
+    aftax_now   += growth*aftax_now;
+    aftax_later += growth*aftax_later;
   }
   frac = holdyrs - (int) holdyrs;
   if (frac > 0.) {
-    rothamt += frac*growth*0.01*rothamt;
-    atamt   += frac*growth*0.01*atamt;
+    roth_now    += frac*growth*roth_now;
+    aftax_now   += frac*growth*aftax_now;
+    aftax_later += frac*growth*aftax_later;
   }
-  attax   = taxrate*0.01*atamt;
-  atfinal = atamt - attax;
-  printf ("Results after holding $%.1lfK for %.1lf years:\n", startamt, holdyrs);
-  printf ("rothamt=$%.2lfK\n", rothamt);
-  printf ("taxes will decrease attamt=$%.2lfK by $%.2lfK to a value of $%.2lfK\n",
-	  atamt, attax, atfinal);
+  now_tax   = (aftax_now - basis)*taxrate;  // tax only growth
+  now_final = aftax_now - now_tax;
+  
+  later_tax   = aftax_later*taxrate;        // tax entirety upon conversion
+  later_final = aftax_later - later_tax;
+    
+  printf ("Converting tradIRA $%.1lfK via 3 methods for %.1lf years:\n", startamt, holdyrs);
+  printf ("roth_now=$%.2lfK Initial reduction (taxes)=$%.2lfK\n", roth_now, init_tax);
+  printf ("now_final=$%.2lfK  Initial reduction (taxes)=$%.2lfK Final reduction (taxes)=$%.2lfK\n",
+	  now_final, init_tax, now_tax);
+  printf ("later_final=$%.2lfK  Final reduction (taxes)=$%.2lfK\n", later_final, later_tax);
+  
   return 0;
 }
